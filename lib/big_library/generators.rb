6f44a79f8fbedc94b27ee84c6179c86c23c9c42b
Tony Stubblebine <tony@crowdvine.com>
2011-03-08 23:32:45 -0500

    Add generator method for copying over migration files.
    - This includes a hack for making sure the timestamps of multiple calls to this end up different.

--- lib/big_library/generators.rb
+++ lib/big_library/generators.rb
@@ -12,6 +12,16 @@ module BigLibrary
     def copy_javascript
       copy_file 'assets/javascripts/rails.js',  'public/javascripts/big_lib_rails.js'
     end
+
+    # Migration is the migration_class_name and the asset file should be named migration_class_name_migration.rb
+    def create_migration_file(migration)
+      begin
+        migration_template "#{migration}_migration.rb", "db/migrate/#{migration}.rb"
+        sleep(2) # cheap hack to make sure migration numbers end up being different
+      rescue
+        puts $!
+      end
+    end
   end # Generators
 end # BigLibrary
 
